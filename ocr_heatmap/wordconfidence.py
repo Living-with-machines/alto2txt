25835ba8e58a7944df609b7567431f0e2e474827
yannryanBL <45659603+yannryanBL@users.noreply.github.com>
2019-01-25 11:16:19 +0000

    Create wordconfidence.py

---
+++ ocr_heatmap/wordconfidence.py
@@ -0,0 +1,14 @@
+#writes the coordinates and cord confidence scores to a text file
+
+from bs4 import BeautifulSoup
+
+
+infile = open("filename.xml","r")
+#change to the name of the ALTO xml file
+
+contents = infile.read()
+soup = BeautifulSoup(contents, 'xml')
+stringblock = soup.find_all('String')
+log = open("coordinates_confidence.txt", "w")
+for s in stringblock:
+	print(s['HPOS'],",",s['VPOS'],",",s['WC'], sep='', file = log)
