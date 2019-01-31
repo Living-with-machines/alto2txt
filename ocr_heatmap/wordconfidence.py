a9bbdee69f7a1baca8edf533de4d6167ba553d2e
yannryanBL <45659603+yannryanBL@users.noreply.github.com>
2019-01-31 10:30:17 +0000

    Update wordconfidence.py

--- ocr_heatmap/wordconfidence.py
+++ ocr_heatmap/wordconfidence.py
@@ -1,14 +1,22 @@
-#writes the coordinates and word confidence scores to a text file
+#requires at least one xml file in the same folder as the python script
+#this script will create a text file with the same filename as each xml file in the directory
+#and write the HPOS, VPOS and WC values to a .csv type output. 
+
 
 from bs4 import BeautifulSoup
+import glob, os
+from tqdm import tqdm
+xmllist = []
+for file in glob.glob("*.xml"):
+    xmllist.append(file)
 
 
-infile = open("filename.xml","r")
-#change to the name of the ALTO xml file
-
-contents = infile.read()
-soup = BeautifulSoup(contents, 'xml')
-stringblock = soup.find_all('String')
-log = open("coordinates_confidence.txt", "w")
-for s in stringblock:
-	print(s['HPOS'],",",s['VPOS'],",",s['WC'], sep='', file = log)
+for d in xmllist:
+	infile = open(d, "r", encoding='utf-8')
+	contents = infile.read()
+	soup = BeautifulSoup(contents, 'xml')
+	stringblock = soup.find_all('String')
+	f = open(( d.rsplit( ".", 1 )[ 0 ] ) + ".txt", "w")
+	for i in tqdm(range(len(xmllist))):	
+		for s in stringblock:
+		print(s['HPOS'],",",s['VPOS'],",",s['WC'], sep='', file = f)
