2c3b3a3ccc4c6428a3bd94a8b0e70fa53b30efb9
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2022-01-27 17:49:50 +0000

    Stubbing out dummy tests

--- tests/e2e.py
+++
@@ -1,5 +0,0 @@
-from alto2txt import extract_publications_text
-
-
-def test_cli():
-    pass
---
+++ tests/test_e2e.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_cli():
+    pass
+
+def test_always_fail():
+    # Dunny tests, just to check that the test framework is finding the right tests
+    assert False
+
+def test_always_pass():
+    # Dunny tests, just to check that the test framework is finding the right tests
+    assert True
